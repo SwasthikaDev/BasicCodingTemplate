d24eafe6e60ac03321b3725818079f69ab49b465
Swasthika <62395158+SwasthikaDev@users.noreply.github.com>
2021-08-14 17:35:46 +0530

    Create Kadane Algo Max sum Subarray.cpp

---
+++ Kadane Algo Max sum Subarray.cpp
@@ -0,0 +1,16 @@
+int maxsum(vector<int>& nums) {
+        if(nums.size()==0) return 0;
+        int sum = nums[0], sumc = nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+           sumc = max(sumc+nums[i],nums[i]);
+           sum = max(sum,sumc);
+        }
+        return sum;
+    }
+int main()
+{
+vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+int sumzz = maxsum(nums);
+cout<<sumzz<<endl;
+}
